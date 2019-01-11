ab87842044752492c89493f4b5f2413bc54fbee9
mpreyes <[reyes.madelyn.mr@gmail.com]>
2019-01-11 13:00:10 -0600

    linked pages together, added images folder

--- bisonMatchApp/views.py
+++ bisonMatchApp/views.py
@@ -22,21 +22,3 @@ def profile(request):
 
 def quiz(request):
     return render(request, 'bisonMatchApp/quiz.html')
-
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/about/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, 'bisonMatchApp/name.html', {'form': form})
