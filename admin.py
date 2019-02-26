2812fc6f156701b6977cc436b6373bdc5f3d14ea
mpreyes <[reyes.madelyn.mr@gmail.com]>
2019-02-26 11:22:37 -0600

    final push of files downloaded from server.

---
+++ admin.py
@@ -0,0 +1,157 @@
+from django.contrib import admin
+from .models import Lustudent, StudentMatches
+from django.http import HttpResponseRedirect
+from django.db import IntegrityError, DatabaseError, connection
+from django.core.mail import send_mail
+from django.template import loader
+
+from contextlib import closing
+from operator import itemgetter
+
+admin.site.disable_action('delete_selected')
+
+numMatches = 5
+totalQuestions = 10
+
+@admin.register(Lustudent)
+class LustudentAdmin(admin.ModelAdmin):
+    list_display = [ 'name', 'lnumber', 'emailaddress', 'gender', 'paid', 'bio','idealdate']
+    ordering = ['lnumber']
+    actions = ['generateMatches','markPaid', 'sendReminderEmail', 'sendResultEmail', 'delete_selected']
+
+    def markPaid(self, request, queryset):
+        try:
+            rows_updated = queryset.update(paid = 1)
+            if rows_updated == 1:
+                message_bit = "1 student was"
+            else:
+                message_bit = "%s students were" % rows_updated
+            self.message_user(request, "%s successfully marked as paid." % message_bit)
+        except DatabaseError:
+            self.message_user(request, "An error occured with your request.")
+        return
+    markPaid.short_description = 'Mark student as paid'
+
+    def generateMatches(self, request, queryset):
+        students = queryset
+        totalInserts = 0
+        for student in students:
+            matches = getMatchesForStudent(student) 
+            totalInserts += len(matches)
+            for match in matches:
+                try:
+                    StudentMatches.objects.create(studentlnumber=student.lnumber, matchlnumber=match['lnumber'], percent=match['percent'])
+                except (IntegrityError, DatabaseError):
+                    self.message_user(request, "An error occured with your request, match %s with %s was not added to StudentMatches"% (student.lnumber, match['lnumber']))
+        self.message_user(request, 'Done: %s rows should have been inserted' %totalInserts)
+        return HttpResponseRedirect("../studentmatches/")
+    generateMatches.short_description = 'Generate Matches for Students (Ensure all selected)'
+
+    def sendReminderEmail(self, request, queryset):
+        students = queryset
+        for student in students:
+            if student.paid:
+                html_message = loader.render_to_string('bisonMatchApp/paid_remind_email.html', {'name': student.name})
+                subject = "BisonMatch Reminder"
+                message = "Hi " + student.name + ",\n"
+                message += "Reminder that your Bison Match results will be available via email Monday February 11, just in time to find a Valentine's Date!\n"
+                message += "Have a wonderful day!\n"
+                message += "Love, \n Your Friends at Bison Match"
+            else: 
+                html_message = loader.render_to_string('bisonMatchApp/unpaid_remind_email.html', {'name': student.name})
+                subject = "Its Not Too Late To Find Love!"
+                message = "Hi " + student.name + ",\nIt's not too late for you to buy your results from Bison Match!\n"
+                message += "Visit bisonmatch.info/bisonMatch/thanks or stop by the Bison Match table in the student center to pay for your results.\n" 
+                message += "Once you have paid, you will receive your results via email on Monday February 11, just in time to find a Valentine's Date!\n"
+                message += "Have a wonderful day!\n"
+                message += "Love, \n Your Friends at Bison Match"
+            #Send email with appropriate subject and message
+            send_mail(
+                subject, #subject
+                message,   #body
+                'bisonmatch2.0@gmail.com', #from us
+                [student.emailaddress], #to student
+                fail_silently=False,
+                html_message=html_message
+            )
+        return
+    sendReminderEmail.short_description = 'Send Reminder to Students (Ensure all selected)'
+    
+    def sendResultEmail(self, request, queryset):
+        students = queryset
+        for student in students:
+            if student.paid == 1 or student.paid == 3:
+                html_message = loader.render_to_string('bisonMatchApp/paid_results_email.html', {'name': student.name, 'lnumber': student.lnumber})
+                subject = "Your Bison Match Results Are Now Available!!!"
+                message = "Hi " + student.name + ",\n"
+                message += "Thank you for supporting your local ACM chapter by buying your results!\n"
+                message += "To view your Bison Match results, go to http://157.230.208.45:8001/bisonmatch/matches" + student.lnumber
+                message += "\nHappy Valentine's Day!\n\nMuch Love,\nYour Friends at Bison Match"
+                send_mail(
+                          subject, #subject
+                          message,   #body
+                          'bisonmatch2.0@gmail.com', #from us
+                          [student.emailaddress], #to student
+                          fail_silently=False,
+                          html_message=html_message
+                          )
+                student.paid = 3
+                student.save()
+            else:
+                html_message = loader.render_to_string('bisonMatchApp/unpaid_results_email.html', {'name': student.name, 'lnumber': student.lnumber})
+                subject = "Get your Bison Match Results Before 1 today!"
+                message = "Hi " + student.name + ",\n"
+                message += "It's not too late to view your Bison Match results!\n"
+                message += "Just go to http://157.230.208.45:8001/thanks to buy your results.\n"
+                message += "Once you have paid for your results, you can view them at http://157.230.208.45:8001/bisonmatch/matches" + student.lnumber
+                message += "\nHappy Valentine's Day!\n\nMuch Love,\nYour Friends at Bison Match"
+                send_mail(
+                    subject, #subject
+                    message,   #body
+                    'bisonmatch2.0@gmail.com', #from us
+                    [student.emailaddress], #to student
+                    fail_silently=False,
+                     html_message=html_message
+                )
+                student.paid = 2
+                student.save()
+        return
+    sendResultEmail.short_description = 'Send Match Link to Students (Ensure all selected)'
+    
+
+
+@admin.register(StudentMatches)
+class StudentMatchesAdmin(admin.ModelAdmin):
+    list_display = ['studentlnumber', 'matchlnumber', 'percent']
+    ordering = ['studentlnumber']
+    actions = ['delete_selected']
+
+
+def getMatchesForStudent(student):
+    pointedStudents = getPotentialMatchesAndPoints(student)
+    # Test sorting
+    sortedStudents = sorted(pointedStudents, key=itemgetter('percent'), reverse=True)
+    finalMatches = sortedStudents[0:numMatches]
+    return finalMatches
+
+def getPotentialMatchesAndPoints(student):
+    #student is male(0) get female(1) matches
+    if student.gender == 0:
+        potentialStudents = Lustudent.objects.filter(gender = 1)
+    #student is female(1) get male(0) matches
+    elif student.gender == 1:
+        potentialStudents = Lustudent.objects.filter(gender = 0)
+    #student is undefined(2) get all students
+    elif student.gender == 2:
+        potentialStudents = Lustudent.objects.exclude(lnumber = student.lnumber)
+        print(potentialStudents)
+    pointedStudents = []
+    qs = ['ans1', 'ans2', 'ans3', 'ans4', 'ans5', 'ans6', 'ans7', 'ans8', 'ans9', 'ans10']
+    for potentialStudent in potentialStudents:
+        points = 0
+        for q in qs:
+            if getattr(student, q) == getattr(potentialStudent, q):
+                points += 1
+        percent = (points/totalQuestions) * 100
+        pointedStudents.append({'lnumber': potentialStudent.lnumber, 'percent': percent})
+    return pointedStudents
---
+++ views.py
@@ -0,0 +1,180 @@
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template import loader
+from django.db import connection
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Lustudent, ImageUpload
+from .forms import ProfileForm, UploadImageForm
+from contextlib import closing
+from django.urls import reverse
+from django.shortcuts import render
+
+import base64
+from django.core.files.base import ContentFile
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'bisonMatchApp/index.html')
+
+def about(request):
+    LU_student = Lustudent.objects.values('name')
+    context = {'LU_student': LU_student}
+    return render(request, 'bisonMatchApp/about.html', context)
+
+def quiz(request):
+    if request.method == 'POST':
+        print("Processing post...")
+        # Check in the terminal for how the session variables are coming in...
+        for key, value in request.POST.items():
+            if key != "image_data":
+                print('{} => {}'.format(key, value))
+
+        MEDIA_ROOT = "/media/user_profiles/"
+        if request.POST["image_data"] != "":
+            image_data = request.POST["image_data"]
+            format, imgstr = image_data.split(';base64,')
+            ext = format.split('/')[-1]
+            file_name = str(request.POST["l-number"]) + "." + ext
+            image = ContentFile(base64.b64decode(imgstr))
+            document = ImageUpload()
+            document.media.save(file_name, image)
+            document.save()
+            image_file_path = MEDIA_ROOT + file_name
+        else:
+            image_file_path = MEDIA_ROOT + "bisonMatchLogo.png"
+	#param1 = request.POST["name"];param2 = request.POST["l-number"];param3 = request.POST["email"];param4 = request.POST["major"]; param5 = request.POST["bio"]; param6 = request.POST["idealdate"];
+
+        sql = "INSERT INTO lustudent VALUES ("
+        sql += "\"" + request.POST["name"] + "\", "
+        sql += "\"" + request.POST["l-number"] + "\", "
+        sql += "\"" + request.POST["email"] + "\", "
+        sql += "\"" + request.POST["major"] + "\", "
+        sql += "\"" + request.POST["bio"] + "\", "
+        sql += "\"" + request.POST["idealdate"] + "\", "
+        sql += "\"" + request.POST["gender"] + "\", "
+        sql += request.POST["question1"] + ", "
+        sql += request.POST["question2"] + ", "
+        sql += request.POST["question3"] + ", "
+        sql += request.POST["question4"] + ", "
+        sql += request.POST["question5"] + ", "
+        sql += request.POST["question6"] + ", "
+        sql += request.POST["question7"] + ", "
+        sql += request.POST["question8"] + ", "
+        sql += request.POST["question9"] + ", "
+        sql += request.POST["question10"] + ", "
+        sql += "\"" + image_file_path + "\", "
+        sql += "0);"
+
+        evilStuff = ["lustudent", "delete", "studentmatches", "*", "DELETE", "DROP TABLE", "drop table", "Addison"]
+        textInputs = [request.POST["name"], request.POST["l-number"], request.POST["email"], request.POST["bio"], request.POST["idealdate"], request.POST["major"] ]
+
+        if any(elem in evilStuff for elem in textInputs):
+               return render(request, 'bisonMatchApp/quiz.html')
+        for text in textInputs:
+               for evil in evilStuff:
+                       if evil in text.lower():
+                               return render(request, 'bisonMatchApp/quiz.html')
+
+        print("here be ye sql statement " + str(sql))
+        #TODO Consider replacing the below to lines with the following
+        with closing(connection.cursor()) as cursor:
+            cursor = connection.cursor()
+            cursor.execute(sql)
+        connection.close()
+        #This ensures that both the cursor and the connection are closed
+
+        lnumbers = getAllStudentNumbers()
+
+        return HttpResponseRedirect('/bisonMatch/thanks/', {"lnumbers" : lnumbers})
+    else:
+        return render(request, 'bisonMatchApp/quiz.html')
+
+@csrf_exempt
+def thanks(request):
+    if request.POST:
+        lnumber = request.POST.get("invoice", "")
+
+        if lnumber != "":
+            with closing(connection.cursor()) as cursor:
+                cursor = connection.cursor()
+                cursor.execute("UPDATE lustudent SET paid=1 WHERE lnumber='" + str(lnumber) + "';")
+            connection.close()
+            print("Updated person with lnumber: " + str(lnumber))
+        else:
+            print("There has been a fatal error...")
+            return None
+        return render(request, 'bisonMatchApp/index.html')
+
+
+    lnumbers = getAllStudentNumbers()
+
+    return render(request, 'bisonMatchApp/thanks.html', {"lnumbers" : lnumbers})
+
+@csrf_exempt
+def payment_finished(request):
+    #sendResult('reyes.madelyn.mr@gmail.com','results')
+    return render(request, 'bisonMatchApp/payment_success.html')
+
+@csrf_exempt
+def payment_error(request):
+    #sendResult('reyes.madelyn.mr@gmail.com','results')
+    return render(request, 'bisonMatchApp/payment_error.html')
+
+
+
+def getStudentData(lnumber):
+    student = None
+    with closing(connection.cursor()) as cursor:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM lustudent where lnumber = '" + str(lnumber) + "';")
+        student = cursor.fetchone()
+    connection.close()
+    return student
+
+def getAllStudentNumbers():
+    lnumbers = []
+    with closing(connection.cursor()) as cursor:
+        cursor = connection.cursor()
+        cursor.execute("SELECT lnumber FROM lustudent;")
+        res = cursor.fetchall()
+    connection.close()
+
+    for obj in res:
+        if obj[0] != "":
+            lnumbers.append(obj[0])
+    return lnumbers
+
+def matches(request, slug):
+    matchLNumbers = []
+    percentages = []
+    paid = 0
+    with closing(connection.cursor()) as cursor:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM studentmatches WHERE studentlnumber = '" + str(slug) + "';")
+        res = cursor.fetchall()
+        #print("res from student matches " + str(res))
+    connection.close()
+    for object in res:
+        matchLNumbers.append(object[2])
+        percentages.append(object[3])
+
+    with closing(connection.cursor()) as cursor:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM lustudent WHERE lnumber = '" + str(slug) + "';")
+        res = cursor.fetchone()
+        #print("res from lu student " + str(res))
+        paid = res[-1]
+    connection.close()
+
+    matches = []
+    i = 0
+    for lnumber in matchLNumbers:
+        matches.append(list(getStudentData(lnumber)) + [percentages[i]])
+        i += 1
+    #print("matches: " + str(matches))
+
+    return render(request, 'bisonMatchApp/matches.html', {"matches" : matches, "paid" : paid})
+
